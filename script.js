7a32424b991ac9a48378ca021efede662ecab455
amekatze <setharie@gmail.com>
2022-08-25 00:23:20 +0900

    Refactored code

--- script.js
+++ script.js
@@ -1,69 +1,78 @@
+let input = '';
+let log;
+let operator = '';
 let x;
 let y;
-let operator;
-let output;
-const display = document.querySelector('.calc-display');
 
+
+const inputDisplay = document.querySelector('.calc-lower');
+const display = document.querySelector('.calc-upper');
+const numberButtons = document.querySelectorAll('#number');
+const operatorButtons = document.querySelectorAll('#operator');
+const equalButton = document.getElementById('equal');
 
 function operate(){
-    if (x == undefined){ 
-        x = 'error';
-        output = x;
-    }
-    else {
-    x = parseInt(x);
-    y = parseInt(y);
-
-    x = 
-    operator == '+' ? (x + y) : 
-    operator == '-' ? (x - y) :
-    operator == '*' ? (x * y) :
-    operator == '/' ? y == 0 ? 'error' : (x / y) : 
-    operator == '%' ? ((x / 100) * y) : x;
-
-    y = undefined;
-    operator = undefined
-    output = x;
+ switch (operator){
+    case '+': 
+        return x+y;
+        break;
+    case '-':
+        return x-y;
+        break;
+    case '*':
+        return x*y;
+        break;
+    case '/':
+        return x/y;
+        break;
+    case '%':
+        return (x/100)*y;
+        break;
+ } 
 }
-}
-
+ 
 function clear(){
-    x = undefined;
-    y = undefined;
-    operator = undefined;
-    output = '';
+    inputDisplay.innerHTML = '';
+    display.innerHTML = '';
+};
+
+numberButtons.forEach(button => {
+    button.onclick = function(){ 
+        input += button.textContent;
+        inputDisplay.innerHTML = input;
+        console.log(input)
+    }})
+
+operatorButtons.forEach(button => {
+    button.onclick = function(){ 
+    if (operator == ''){
+        console.log('hi');
+        x = parseFloat(input);
+        operator = button.textContent;
+        display.innerHTML = x + operator;
+        inputDisplay.innerHTML = '';
+        input = ''; 
+    } else if (input == ''){
+        operator = button.textContent;
+        display.innerHTML = x + operator;
+    } else {
+        y = parseFloat(input);
+        log = operate();
+        inputDisplay.innerHTML = '';
+        operator = button.textContent;
+        display.innerHTML = log + operator;
+        input = '';
+        x = log;
+    } 
+    console.log(x,operator,y)
+    }})
+     
+equalButton.onclick = function() {
+    y = parseFloat(input);
+    log = x + operator + y;
+    input = operate();
+    display.innerHTML = log;
+    inputDisplay.innerHTML = input;
+    operator = '';
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-    button.onclick = function(){
-     if (this.id == 'number'){
-        if (operator == undefined){
-            x = x == undefined || typeof(x) == 'number' || x == 'error' ? this.value : x + this.value;
-            output = x;
-        } else {
-            y = y == undefined ? this.value : y + this.value;
-            output = x + operator + y;
-        }
-     } 
-     if (this.id == 'operator'){
-        x = x == undefined ? 0 : x;
-        if (operator == undefined){
-            operator = this.value;
-            output += operator;
-        } else if (y != undefined){
-           operate(); 
-           operator = this.value;
-           output += operator;
-        } else {
-            operator = this.value;
-            output = x + operator;
-        }
-     }
-
-     if (this.id == 'equal'){ operate();}
-     if (this.id == 'clear'){ clear();}
-     console.log(x, operator, y);
-     display.innerHTML = output;
-    }
-})
